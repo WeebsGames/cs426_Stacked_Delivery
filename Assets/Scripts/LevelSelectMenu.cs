a6bbab8a91d45b0d58f177032bd1e217e4b3a4c1
Syed Daanish Ismail <sismai20@uic.edu>
2026-03-20 12:44:23 -0500

    level 1 loads proto

--- Assets/Scripts/LevelSelectMenu.cs
+++ Assets/Scripts/LevelSelectMenu.cs
@@ -6,6 +6,7 @@ public class LevelSelectMenu : MonoBehaviour
     public void LoadLevel1()
     {
         Debug.Log("Level 1 clicked");
+        SceneManager.LoadScene("Scene1");
     }
 
     public void LoadLevel2()

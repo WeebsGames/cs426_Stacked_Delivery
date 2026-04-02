d5857c470910be250330241cab6b9bd7dafde6e1
AreebJ <68392559+WeebsGames@users.noreply.github.com>
2026-04-02 01:08:54 -0500

    updated build

--- Assets/Scripts/AE86Engine.cs
+++ Assets/Scripts/AE86Engine.cs
@@ -4,8 +4,8 @@ public class AE86Engine : MonoBehaviour
 {
     public AudioSource source;
     public Rigidbody rb;
-    public AudioClip idle;
-    public AudioClip running;
+    // public AudioClip idle;
+    // public AudioClip running;
     float idleStart = 54f;
     float idleEnd = 64f;
     bool idling = false;
@@ -13,7 +13,9 @@ public class AE86Engine : MonoBehaviour
     void Start()
     {
         // source.timeSamples = (int)(loopStart * source.clip.frequency);
-        source.clip = running;
+        // Resources.Load("Assets/Audio/Initial D Engine SFX  Initial D Collectors Disc.mp3");
+        // Resources.Load("Assets/PROMETEO - Car Controller/Sounds/CarEngine.wav");
+        // source.clip = running;
         source.Play();
         source.loop = true;
     }
@@ -23,30 +25,30 @@ public class AE86Engine : MonoBehaviour
     {
         source.pitch = 0.5f + rb.linearVelocity.magnitude/12f;
 
-        // source.volume = rb.linearVelocity.magnitude;
-        if(rb.linearVelocity.magnitude == 0)
-        {
-            // idling = true;
-            // source.volume = 1f;
-            source.clip = idle;
-        }
-        else
-        {
-            // idling = false;
-            source.clip = running;
-        }
+        // // source.volume = rb.linearVelocity.magnitude;
+        // if(rb.linearVelocity.magnitude == 0)
+        // {
+        //     // idling = true;
+        //     // source.volume = 1f;
+        //     source.clip = idle;
+        // }
+        // else
+        // {
+        //     // idling = false;
+        //     source.clip = running;
+        // }
     }
 
-    void idleSwitch()
-    {
-        source.Stop();
-        source.clip = idle;
-        source.volume = 1f;
-        source.Play();
-        // Reset to loop start if the current time exceeds the loop end
-        if (source.time >= idleEnd)
-        {
-            source.timeSamples = (int)(idleEnd * source.clip.frequency);
-        }
-    }
+    // void idleSwitch()
+    // {
+    //     source.Stop();
+    //     source.clip = idle;
+    //     source.volume = 1f;
+    //     source.Play();
+    //     // Reset to loop start if the current time exceeds the loop end
+    //     if (source.time >= idleEnd)
+    //     {
+    //         source.timeSamples = (int)(idleEnd * source.clip.frequency);
+    //     }
+    // }
 }

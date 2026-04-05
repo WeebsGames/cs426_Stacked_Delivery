0afef7e3bc3291df5399af3d35be73e2b6f90f15
AreebJ <68392559+WeebsGames@users.noreply.github.com>
2026-04-04 20:36:13 -0500

    esc now pulls up pause menu

--- Assets/Scripts/MenuScripts/PauseMenu.cs
+++ Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -1,20 +1,23 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
     public Transform pausePos;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject cam;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            if(cam.GetComponent<CameraFollow>().state == CameraFollow.State.PlayState)
+            {
+                cam.GetComponent<CameraFollow>().state = CameraFollow.State.PauseState;
+            } else
+            {
+                cam.GetComponent<CameraFollow>().state = CameraFollow.State.PlayState;
+            }
             
         }
     }

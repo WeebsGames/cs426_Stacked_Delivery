9319a1430873cb0a0a979f08a671db69fcbad1d3
AreebJ <68392559+WeebsGames@users.noreply.github.com>
2026-04-08 14:29:08 -0500

    level orbs now highlight when they are clickable

--- Assets/Scripts/MenuScripts/LvlOrbScript.cs
+++ Assets/Scripts/MenuScripts/LvlOrbScript.cs
@@ -4,9 +4,45 @@ using UnityEngine.SceneManagement;
 public class LvlOrbScript : MonoBehaviour
 {
     public int targetLvl;
+    public bool locked;
+    public Material unlockedMat;
+    public Material lockedMat;
+    public Material selected;
+
+    Renderer render;
+
+    void Start()
+    {
+        render = GetComponent<Renderer>();
+        CheckMat();
+    }
+
+    void OnMouseOver()
+    {
+        if(locked == false)
+        {
+            render.material = selected;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        CheckMat();
+    }
 
     void OnMouseDown()
     {
         SceneManager.LoadScene("Track" + targetLvl);
+    }
+
+    void CheckMat()
+    {
+        if (locked)
+        {
+            render.material = lockedMat;
+        } else
+        {
+            render.material = unlockedMat;
+        }
     }
 }

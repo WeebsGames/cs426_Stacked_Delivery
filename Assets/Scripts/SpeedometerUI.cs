c98d147319fb052cf3b083f9a6d0a8015e875b8c
asalga24 <77249837+asalga24@users.noreply.github.com>
2026-04-09 11:00:53 -0500

    Add speedometer UI
    - reflects top speed of carcontrol 20f

---
+++ Assets/Scripts/SpeedometerUI.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SpeedometerUI : MonoBehaviour
+{
+    [Header("References")]
+    public RectTransform needle;
+    public Rigidbody carRigidbody;
+
+    [Header("Needle Settings")]
+    public float minAngle = 217f;
+    public float maxAngle = -40f;
+    public float maxSpeed = 20f;
+
+    void Update()
+    {
+        float speed = carRigidbody.linearVelocity.magnitude;
+        float t = Mathf.Clamp01(speed / maxSpeed);
+        float angle = Mathf.Lerp(minAngle, maxAngle, t);
+
+        needle.localEulerAngles = new Vector3(0f, 0f, angle);
+    }
+}
